a40dd6043a6bc686cbbae0fff108b8c6193a1e00
Michał Wziątek <michalwziatek@icloud.com>
2019-03-20 14:04:16 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Michał Wziątek on 20/03/2019.
+//  Copyright © 2019 Michał Wziątek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
